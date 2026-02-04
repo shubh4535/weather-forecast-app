02b62471dafd97cfae21337813542a7fb2397f42
Shubham Singh <shubh4535@gmail.com>
2026-02-04 10:08:51 +0530

    Implemented async city-based weather fetch with error handling

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const API_KEY = "95219ab4844c9a53a98448897a670227";
+
+async function getWeatherByCity(city){
+    try {
+        const res = await fetch(
+           `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+        )
+        if (!res.ok) throw new Error("City not found");
+        const data = await res.json();
+        displayWeather(data);
+    }
+    catch (error) {
+        showError(error.message);
+    }
+}
